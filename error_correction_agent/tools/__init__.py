95559e0f22f21313c3a28e5e55688d2c2e952049
zheji <3077807213@qq.com>
2026-02-07 20:30:06 +0800

    导出 split_batch

--- error_correction_agent/tools/__init__.py
+++ error_correction_agent/tools/__init__.py
@@ -2,12 +2,13 @@
 错题本生成Agent工具集
 """
 
-from .question_tools import save_questions, log_issue
+from .question_tools import save_questions, log_issue, split_batch
 from .file_tools import download_image, read_ocr_result
 
 __all__ = [
     "save_questions",
     "log_issue",
+    "split_batch",
     "download_image",
     "read_ocr_result",
 ]
